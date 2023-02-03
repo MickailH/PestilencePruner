843c9d38dcecf051c29c6048a17f092638e9b795
Zehui Liu/Enrico <liuzehui1849@gmail.com>
2023-02-03 21:42:03 +0000

    Basic movements

---
+++ PestilencePruner/Assets/Scripts/Characters/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float jumpForce = 5;
+    private Rigidbody2D rb;
+    private Collider2D collider;
+    private Vector2 movement;
+    public bool onGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        
+        if(Input.GetKeyDown("space") && onGround) {
+            onGround = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        //TODO: Fix the bug that the character will stick on the square when pressing "a" or "d" when jumping over it
+        rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Floor")){
+            onGround = true;
+        }
+    }
+
+}
