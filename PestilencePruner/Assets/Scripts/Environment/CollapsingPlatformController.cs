31bf4e19637538e6a69038651115bda446b947b2
MickailHemono <mickailhemono@gmail.com>
2023-02-04 22:37:16 +0000

    Fixed collapsing platform bug

--- PestilencePruner/Assets/Scripts/Environment/CollapsingPlatformController.cs
+++ PestilencePruner/Assets/Scripts/Environment/CollapsingPlatformController.cs
@@ -5,26 +5,29 @@ using UnityEngine;
 public class CollapsingPlatformController : PlatformController
 {
     private Rigidbody2D rb;
-    private Collider2D collider;
+    //private Collider2D collider;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        collider = GetComponent<Collider2D>();
+        // collider = GetComponent<Collider2D>();
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        //TODO: Only trigger when the character is above the platform
-       // if (transform.position.y + collider.bounds.extents.y < other.gameObject.position.y - other.bounds.extents.y)
-        //{
-            if (other.gameObject.name.Equals("Character"))
-            {
+        if (other.gameObject.name.Equals("Character"))
+        {
+            Collider2D col = other.collider;
+            Transform player = other.transform;
+
+            if(player.position.y - col.bounds.extents.y > transform.position.y + transform.GetComponent<Collider2D>().bounds.extents.y){
+                print("jump reset");
+                col.GetComponent<PlayerMovement>().HitGround();
                 //Invoke("DropPlatform", 0.5f);
                 Destroy(gameObject, 2f);
                 PlatformManager.Instance.StartCoroutine("SpawnPlatform", new Vector2(transform.position.x, transform.position.y));
             }
-       // }
+        }
     }
 
 
